e63ab21f017d8d99a49777cd120fcf746e39d3b3
Kaloyan Kolev <kaloyan21kolev@gmail.com>
2023-07-27 18:46:59 +0300

    Add Routing module

--- ExamProject/src/app/app.module.ts
+++ ExamProject/src/app/app.module.ts
@@ -2,22 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { TodolistComponent } from './todolist/todolist.component';
-import { PopupComponent } from './popup/popup.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { VehicleCarCardComponent } from './vehicle-car-card/vehicle-car-card.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TodolistComponent,
-    PopupComponent,
     NavigationComponent,
     VehicleCarCardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
